a4b460f5a65c99dba0d8c4e03364e0baf9f86e92
Bhoraj1 <journeyn556@gmail.com>
2024-12-11 13:35:23 +0545

    complete create post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,15 +1,53 @@
-import { Button, FileInput, Select, TextInput } from "flowbite-react";
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useNavigate } from "react-router-dom";
 
 export default function CreatePost() {
+  const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch("/api/post/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      } else {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-3xl min-h-screen mx-auto ">
       <h1 className="text-3xl font-semibold text-center my-7">Create Post</h1>
-      <form className="flex flex-col gap-4 ">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
-          <TextInput type="text" placeholder="Title" required className="flex-1" />
-          <Select>
+          <TextInput
+            type="text"
+            placeholder="Title"
+            required
+            className="flex-1"
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
+          />
+          <Select
+            onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
+          >
             <option value="uncategorized">Select a Category</option>
             <option value="1">Category 1</option>
             <option value="2">Category 2</option>
@@ -17,14 +55,32 @@ export default function CreatePost() {
           </Select>
         </div>
         <div className="flex items-center justify-between gap-4 border-4 border-teal-500 border-dotted p-3 ">
-          <FileInput type='text' accept="image/*" />
-          <Button type="submit" gradientDuoTone="purpleToBlue" outline size="sm">
+          <FileInput type="text" accept="image/*" />
+          <Button
+            type="submit"
+            gradientDuoTone="purpleToBlue"
+            outline
+            size="sm"
+          >
             Upload Image
           </Button>
-        </div> 
-        <ReactQuill theme="snow" className="h-72 mb-12" placeholder="write something..." required />
-        <Button type="submit" gradientDuoTone="purpleToPink">Publish</Button>
+        </div>
+        <ReactQuill
+          theme="snow"
+          className="h-72 mb-12"
+          placeholder="write something..."
+          required
+          onChange={(value) => setFormData({ ...formData, content: value })}
+        />
+        <Button type="submit" gradientDuoTone="purpleToPink">
+          Publish
+        </Button>
+        {publishError && (
+          <Alert color="failure" className="mt-5">
+            {publishError}
+          </Alert>
+        )}
       </form>
     </div>
-  )
+  );
 }
